10a08f6e9a035bcd51f71a9aac12e06bb2d34c21
William Noll <williampnoll@gmail.com>
2016-03-14 14:09:08 -0400

    Add Updating, Removal, and Completion Features to Tasks

--- App.jsx
+++ App.jsx
@@ -1,16 +1,55 @@
 // App component - represents the whole app
 App = React.createClass({
-  getTasks() {
-    return [
-      { _id: 1, text: "This is task 1" },
-      { _id: 2, text: "This is task 2" },
-      { _id: 3, text: "This is task 3" }
-    ];
+
+  // This mixin makes the getMeteorData method work
+  mixins: [ReactMeteorData],
+
+  getInitialState() {
+      return {
+        hideCompleted: false
+      };
+  },
+
+  // Loads items from the Tasks collection and puts them on this.data.tasks
+  getMeteorData() {
+    let query = {};
+
+    if (this.state.hideCompleted) {
+      // If hide completed is checked, filter tasks
+      query = { checked: { $ne: true }};
+    }
+
+    return {
+      tasks: Tasks.find(query, { sort: { createdAt: -1 }}).fetch(),
+      incompleteCount: Tasks.find({ checked: { $ne: true }}).count()
+    }
   },
 
   renderTasks() {
-    return this.getTasks().map((task) => {
+    // Get tasks from this.data.tasks
+    return this.data.tasks.map((task) => {
       return <Task key={task._id} task={task} />;
+    });
+  },
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // Find the text field via the React ref
+    var text = React.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() // current time
+    });
+
+    // Clear form
+    React.findDOMNode(this.refs.textInput).value = "";
+  },
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: ! this.state.hideCompleted
     });
   },
 
@@ -18,7 +57,23 @@ App = React.createClass({
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({ this.data.incompleteCount })</h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly={true}
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted} />
+            Hide Completed Tasks
+          </label>
+
+          <form className="new-task" onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks" />
+          </form>
         </header>
 
         <ul>
--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -1,3 +1,6 @@
+// Define a collection to hold our tasks
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   Meteor.startup(function () {
     React.render(<App />, document.getElementById("render-target"));
