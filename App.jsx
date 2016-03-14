840cedbb3d45ba7de31a1c9067889878f7757707
William Noll <williampnoll@gmail.com>
2016-03-14 14:48:33 -0400

    Add Meteor User Accounts

--- App.jsx
+++ App.jsx
@@ -21,7 +21,8 @@ App = React.createClass({
 
     return {
       tasks: Tasks.find(query, { sort: { createdAt: -1 }}).fetch(),
-      incompleteCount: Tasks.find({ checked: { $ne: true }}).count()
+      incompleteCount: Tasks.find({ checked: { $ne: true }}).count(),
+      currentUser: Meteor.user()
     }
   },
 
@@ -40,7 +41,9 @@ App = React.createClass({
 
     Tasks.insert({
       text: text,
-      createdAt: new Date() // current time
+      createdAt: new Date(),            // current time
+      owner: Meteor.userId(),           // _id of logged in user
+      username: Meteor.user().username  // username of logged in user
     });
 
     // Clear form
@@ -68,12 +71,16 @@ App = React.createClass({
             Hide Completed Tasks
           </label>
 
-          <form className="new-task" onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              ref="textInput"
-              placeholder="Type to add new tasks" />
-          </form>
+          <AccountsUIWrapper />
+
+          { this.data.currentUser ?
+            <form className="new-task" onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                ref="textInput"
+                placeholder="Type to add new tasks" />
+            </form> : ''
+          }
         </header>
 
         <ul>
